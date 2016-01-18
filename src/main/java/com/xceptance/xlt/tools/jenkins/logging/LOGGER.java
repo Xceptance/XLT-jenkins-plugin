443a9b8c536910f03fd9ccef3f9ecb3d2c0f6583
rstraub <r.straub@xceptance.net>
2016-01-18 16:28:06 +0100

    renamed logger

--- src/main/java/com/xceptance/xlt/tools/jenkins/logging/LOGGER.java
+++ src/main/java/com/xceptance/xlt/tools/jenkins/logging/LOGGER.java
@@ -3,11 +3,9 @@ package com.xceptance.xlt.tools.jenkins.logging;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import com.xceptance.xlt.tools.jenkins.XltDescriptor;
-
 public class LOGGER
 {
-    private static final String LOGGER_NAME = XltDescriptor.PLUGIN_NAME;
+    private static final String LOGGER_NAME = "XltPlugin";
 
     private static final Logger LOGGER = Logger.getLogger(LOGGER_NAME);
 
